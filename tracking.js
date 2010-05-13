e7041857eaf169b3bf74ab2d8c8c34b497dbe440
Kenley Cheung <ken@lordkenthegreat.com>
2010-05-13 17:15:38 -0400

    Everything displays now

--- tracking.js
+++ tracking.js
@@ -1,5 +1,13 @@
 // TODO: Implement saved initial location
 var central_campus = new google.maps.LatLng(42.73, -73.6775);
+var stops_url = "http://shuttles.rpi.edu/shuttle_tracking.kml";
+var west_route_url = "http://shuttles.rpi.edu/routes/1.kml";
+var east_route_url = "http://shuttles.rpi.edu/routes/2.kml";
+var shuttles_url = "http://shuttles.rpi.edu/positions/current.kml";
+var stops_layer = null;
+var west_route = null;
+var east_route = null;
+var shuttles = null;
 
 // Call this function when the page has been loaded
 function initialize()
@@ -10,6 +18,10 @@ function initialize()
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
   var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  stops_layer = new google.maps.KmlLayer(stops_url, { map: map });
+  west_route = new google.maps.KmlLayer(west_route_url, { map: map });
+  east_route = new google.maps.KmlLayer(east_route_url, { map: map });
+  shuttles = new google.maps.KmlLayer(shuttles_url, { map: map});
 }
 
 function geolocate(map)
